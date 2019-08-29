7191151a1d5fa418f4b933cd52a5869697b46aa6
casali1 <frank_casali@hotmail.com>
2019-08-29 14:40:32 -0600

    Added more functionality.

--- MacGuffinsRUs/Program.cs
+++ MacGuffinsRUs/Program.cs
@@ -13,9 +13,6 @@ namespace MacGuffinsRUs
             //const string photoPath = "Photos/";
             //const string categoryPath = "Categories/";
 
-
-            //var currentDirectory = Environment.CurrentDirectory;
-
             var index = Environment.CurrentDirectory.IndexOf("Code");
             var currentDirectory = Environment.CurrentDirectory.Substring(0, index);
             var photoPath = Path.Combine(currentDirectory, @"Base\\Photos\\");
@@ -28,18 +25,85 @@ namespace MacGuffinsRUs
 
             foreach(var file in files)
             {
-                var indexOfFile = file.IndexOf("Photos\\");
+                var indexOfFile = file.IndexOf("Photos\\\\");
                 var fileNameJPG = file.Remove(0, indexOfFile);
                 fileNameJPG = fileNameJPG.Replace("Photos\\\\", "");
                 var fileName = fileNameJPG.Replace(".jpg", "");
 
-                foreach(var dir in dirs)
+                var hasFileMoved = false;
+
+                //Check for ProductNumber-Size-Color folders
+                foreach (var dir in dirs)
                 {
-                    if(dir.ToLower().Contains(fileName.ToLower()))
+                    var indexOfCat = dir.IndexOf("Categories\\\\");
+                    var category = dir.Remove(0, indexOfFile);
+                    category = category.Replace("Categories\\\\", "");
+                 
+                    if (category.ToLower() == fileName.ToLower())
                     {
                         var destFile = Path.Combine(dir, fileNameJPG);
-                        System.IO.File.Copy(file, destFile, true);
+                        File.Move(file, destFile);
+                        hasFileMoved = true;
                         break;
+                    }
+                }
+
+                //Check for ProductNumber-Size and Product-Color folders
+                if (!hasFileMoved)
+                {
+                    foreach (var dir in dirs)
+                    {
+                        var indexOfCat = dir.IndexOf("Categories\\\\");
+                        var category = dir.Remove(0, indexOfFile);
+                        category = category.Replace("Categories\\\\", "");
+
+                        var fileElements = fileName.Split('-');
+
+                        //Check for ProductNumber-Size folders
+                        if (fileElements.Length > 2)
+                        {
+                            if (fileElements[1].ToLower() == "x") fileElements[1] = fileElements[1] + "-" + fileElements[2];  //Accounts for x.
+                            if (fileElements.Length > 3) fileElements[2] = fileElements[3];                                   //Accounts for a 4th element.
+                      
+                            if (category.ToLower() == (fileElements[0] + "-" + fileElements[1]).ToLower() 
+                                || category.ToLower() == (fileElements[0] + "-" + fileElements[2]).ToLower())
+                            {
+                                var destFile = Path.Combine(dir, fileNameJPG);
+                                File.Move(file, destFile);
+                                hasFileMoved = true;
+                                break;
+                            }
+                        }
+                        else if(fileElements.Length == 2)
+                        {
+                            if (category.ToLower() == (fileElements[0] + "-" + fileElements[1]).ToLower())
+                            {
+                                var destFile = Path.Combine(dir, fileNameJPG);
+                                File.Move(file, destFile);
+                                hasFileMoved = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                //Check for ProductNumber folders
+                if (!hasFileMoved)
+                {
+                    foreach (var dir in dirs)
+                    {
+                        var indexOfCat = dir.IndexOf("Categories\\\\");
+                        var category = dir.Remove(0, indexOfFile);
+                        category = category.Replace("Categories\\\\", "");
+
+                        var fileElements = fileName.Split('-');
+
+                        if (category.ToLower() == fileElements[0].ToLower())
+                        {
+                            var destFile = Path.Combine(dir, fileNameJPG);
+                            File.Move(file, destFile);
+                            break;
+                        }
                     }
                 }
             }
