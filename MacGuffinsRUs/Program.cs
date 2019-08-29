c22cd03e2ac8acc710f33f05857511b06027f2cc
casali1 <frank_casali@hotmail.com>
2019-08-29 11:16:07 -0600

    Add project files.

---
+++ MacGuffinsRUs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+
+namespace MacGuffinsRUs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            const string photoPath = "Photos/";
+            const string categoryPath = "Categories/";
+
+            
+
+            if (Directory.Exists(photoPath) && Directory.Exists(categoryPath))
+            {
+
+
+            }
+            else
+            {
+                Console.WriteLine("Cannot find Photos or Categories directory, please make sure the exe is in the same root as these folders.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
