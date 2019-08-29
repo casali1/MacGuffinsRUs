d36f6da473038436ef96d46af78b0407577427f0
casali1 <frank_casali@hotmail.com>
2019-08-29 15:46:16 -0600

    Refactoring.

--- MacGuffinsRUs/Program.cs
+++ MacGuffinsRUs/Program.cs
@@ -9,9 +9,10 @@ namespace MacGuffinsRUs
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
             //const string photoPath = "Photos/";
             //const string categoryPath = "Categories/";
+            var program = new Program();
 
             var index = Environment.CurrentDirectory.IndexOf("Code");
             var currentDirectory = Environment.CurrentDirectory.Substring(0, index);
@@ -23,96 +24,19 @@ namespace MacGuffinsRUs
 
             var dirs = Directory.GetDirectories(categoriesPath);
 
-            foreach(var file in files)
+            foreach (var file in files)
             {
-                var indexOfFile = file.IndexOf("Photos\\\\");
-                var fileNameJPG = file.Remove(0, indexOfFile);
-                fileNameJPG = fileNameJPG.Replace("Photos\\\\", "");
-                var fileName = fileNameJPG.Replace(".jpg", "");
-
                 var hasFileMoved = false;
 
-                //Check for ProductNumber-Size-Color folders
-                foreach (var dir in dirs)
-                {
-                    var indexOfCat = dir.IndexOf("Categories\\\\");
-                    var category = dir.Remove(0, indexOfFile);
-                    category = category.Replace("Categories\\\\", "");
-                 
-                    if (category.ToLower() == fileName.ToLower())
-                    {
-                        var destFile = Path.Combine(dir, fileNameJPG);
-                        File.Move(file, destFile);
-                        hasFileMoved = true;
-                        break;
-                    }
-                }
+                //Check for ProductNumber-Size-Color files
+                hasFileMoved = program.CheckProductNumberSizeColor(dirs, file);
 
-                //Check for ProductNumber-Size and Product-Color folders
-                if (!hasFileMoved)
-                {
-                    foreach (var dir in dirs)
-                    {
-                        var indexOfCat = dir.IndexOf("Categories\\\\");
-                        var category = dir.Remove(0, indexOfFile);
-                        category = category.Replace("Categories\\\\", "");
+                //Check for ProductNumber-Size files and Product-Color files
+                if (!hasFileMoved) hasFileMoved = program.CheckProductNumberSizeOrProductColor(dirs, file);
 
-                        var fileElements = fileName.Split('-');
-
-                        //Check for ProductNumber-Size folders
-                        if (fileElements.Length > 2)
-                        {
-                            if (fileElements[1].ToLower() == "x") fileElements[1] = fileElements[1] + "-" + fileElements[2];  //Accounts for x.
-                            if (fileElements.Length > 3) fileElements[2] = fileElements[3];                                   //Accounts for a 4th element.
-                      
-                            if (category.ToLower() == (fileElements[0] + "-" + fileElements[1]).ToLower() 
-                                || category.ToLower() == (fileElements[0] + "-" + fileElements[2]).ToLower())
-                            {
-                                var destFile = Path.Combine(dir, fileNameJPG);
-                                File.Move(file, destFile);
-                                hasFileMoved = true;
-                                break;
-                            }
-                        }
-                        else if(fileElements.Length == 2)
-                        {
-                            if (category.ToLower() == (fileElements[0] + "-" + fileElements[1]).ToLower())
-                            {
-                                var destFile = Path.Combine(dir, fileNameJPG);
-                                File.Move(file, destFile);
-                                hasFileMoved = true;
-                                break;
-                            }
-                        }
-                    }
-                }
-
-                //Check for ProductNumber folders
-                if (!hasFileMoved)
-                {
-                    foreach (var dir in dirs)
-                    {
-                        var indexOfCat = dir.IndexOf("Categories\\\\");
-                        var category = dir.Remove(0, indexOfFile);
-                        category = category.Replace("Categories\\\\", "");
-
-                        var fileElements = fileName.Split('-');
-
-                        if (category.ToLower() == fileElements[0].ToLower())
-                        {
-                            var destFile = Path.Combine(dir, fileNameJPG);
-                            File.Move(file, destFile);
-                            break;
-                        }
-                    }
-                }
+                //Check for ProductNumbers
+                if (!hasFileMoved) program.CheckProductNumber(dirs, file);             
             }
-
-            //string[] directories = Directory.GetDirectories("\\\\MacGuffins\\Base", "Photos", SearchOption.AllDirectories);
-            //var aaa = Directory.GetFiles("\\MacGuffins\\Base\\Photos");
-            //var dirs = Directory.GetDirectories("Photos");
-            //string strPath = Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory);
-            //var dirs = Directory.GetDirectories("C:\\Users\\frank\\Desktop\\Experiments\\MacGuffins\\Base")
 
             //if (Directory.Exists(photoPath) && Directory.Exists(categoryPath))
             //{
@@ -126,5 +50,107 @@ namespace MacGuffinsRUs
             //}
 
         }
+
+
+
+        #region Helper functions
+     
+        public bool CheckProductNumberSizeColor(string[] dirs, string file)
+        {
+            var indexOfFile = file.IndexOf("Photos\\\\");
+            var fileNameJPG = file.Remove(0, indexOfFile);
+            fileNameJPG = fileNameJPG.Replace("Photos\\\\", "");
+            var fileName = fileNameJPG.Replace(".jpg", "");
+
+            var hasFileMoved = false;
+            foreach (var dir in dirs)
+            {
+                var indexOfCat = dir.IndexOf("Categories\\\\");
+                var category = dir.Remove(0, indexOfCat);
+                category = category.Replace("Categories\\\\", "");
+
+                if (category.ToLower() == fileName.ToLower())
+                {
+                    var destFile = Path.Combine(dir, fileNameJPG);
+                    File.Move(file, destFile);
+                    hasFileMoved = true;
+                    break;
+                }
+            }
+
+            return hasFileMoved;
+        }
+
+        public bool CheckProductNumberSizeOrProductColor(string[] dirs, string file)
+        {
+            var indexOfFile = file.IndexOf("Photos\\\\");
+            var fileNameJPG = file.Remove(0, indexOfFile);
+            fileNameJPG = fileNameJPG.Replace("Photos\\\\", "");
+            var fileName = fileNameJPG.Replace(".jpg", "");
+
+            var hasFileMoved = false;
+            foreach (var dir in dirs)
+            {
+                var category = GetCategory(dir);
+
+                var fileElements = fileName.Split('-');
+
+                if (fileElements.Length > 2)
+                {
+                    if (fileElements[1].ToLower() == "x") fileElements[1] = fileElements[1] + "-" + fileElements[2];  //Accounts for x.
+                    if (fileElements.Length > 3) fileElements[2] = fileElements[3];                                   //Accounts for a 4th element.
+
+                    if (category.ToLower() == (fileElements[0] + "-" + fileElements[1]).ToLower()
+                        || category.ToLower() == (fileElements[0] + "-" + fileElements[2]).ToLower())
+                    {
+                        var destFile = Path.Combine(dir, fileNameJPG);
+                        File.Move(file, destFile);
+                        hasFileMoved = true;
+                        break;
+                    }
+                }
+                else if (fileElements.Length == 2)
+                {
+                    if (category.ToLower() == (fileElements[0] + "-" + fileElements[1]).ToLower())
+                    {
+                        var destFile = Path.Combine(dir, fileNameJPG);
+                        File.Move(file, destFile);
+                        hasFileMoved = true;
+                        break;
+                    }
+                }
+            }
+
+            return hasFileMoved;
+        }
+
+        public void CheckProductNumber(string[] dirs, string file)
+        {
+            var indexOfFile = file.IndexOf("Photos\\\\");
+            var fileNameJPG = file.Remove(0, indexOfFile);
+            fileNameJPG = fileNameJPG.Replace("Photos\\\\", "");
+            var fileName = fileNameJPG.Replace(".jpg", "");
+
+            foreach (var dir in dirs)
+            {
+                var fileElements = fileName.Split('-');
+
+                if (GetCategory(dir).ToLower() == fileElements[0].ToLower())
+                {
+                    var destFile = Path.Combine(dir, fileNameJPG);
+                    File.Move(file, destFile);
+                    break;
+                }
+            }
+        }
+
+        public string GetCategory(string dir)
+        {
+            var indexOfCat = dir.IndexOf("Categories\\\\");
+            var category = dir.Remove(0, indexOfCat);
+            return category.Replace("Categories\\\\", "");
+        }
+
+        #endregion
     }
 }
